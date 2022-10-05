13de6b83e31ac8d5dff874734e2a4242211bd68d
doanduc2000 <doanminhducvip2000@gmail.com>
2022-10-05 15:31:48 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import Login from "./components/Login.js";
-// import Pagination from "./components/Pagination.js";
-// import Search from "./components/Search.js";
 
-// const search = new Search()
-// const pagination = new Pagination(5)
 const login = new Login()
 
-// document.getElementById('root').appendChild(search.render())
-// document.getElementById('root').appendChild(pagination.render())
 document.getElementById('root').appendChild(login.render())
