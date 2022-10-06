e0bbf0a8bb50e58148844d6ec9f4f09d1303784e
doanduc2000 <doanminhducvip2000@gmail.com>
2022-10-06 14:46:45 +0700

    first commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,43 @@
 import { getComponent } from "../util/getComponent.js";
 
 class Register {
+    $bg;
     $container;
+    $row;
+    $col;
+    $card;
+    $cardBody;
+    $title;
+    $text;
+    $btnGroup;
+    $btnRegister;
     $backBtn;
 
     constructor() {
+        this.$bg = document.createElement('div')
+        this.$bg.className = 'bg-light min-vh-100 d-flex flex-row align-items-center dark:bg-transparent'
+
         this.$container = document.createElement('div')
         this.$container.classList.add('container')
+
+        this.$row = document.createElement('div')
+        this.$row.className = 'row justify-content-center'
+
+        this.$col = document.createElement('div')
+        this.$col.className = 'col-md-6'
+
+        this.$card = document.createElement('div')
+        this.$card.className = 'card mb-4 mx-4'
+
+        this.$cardBody = document.createElement('div')
+        this.$cardBody.className = 'card-body p-4'
+
+        this.$title = document.createElement('h1')
+        this.$title.innerHTML = 'Register'
+
+        this.$text = document.createElement('p')
+        this.$text.className = 'text-medium-emphasis'
+        this.$text.innerHTML = 'Create your account'
 
         this.$backBtn = document.createElement('button')
         this.$backBtn.className = 'btn btn-link px-0'
@@ -21,8 +52,15 @@ class Register {
         getComponent()
     }
     render() {
-        this.$container.appendChild(this.$backBtn)
-        return this.$container
+        this.$bg.appendChild(this.$container)
+        this.$container.appendChild(this.$row)
+        this.$row.appendChild(this.$col)
+        this.$col.appendChild(this.$card)
+        this.$card.appendChild(this.$cardBody)
+        this.$cardBody.appendChild(this.$title)
+        this.$cardBody.appendChild(this.$text)
+        this.$cardBody.appendChild(this.$backBtn)
+        return this.$bg
     }
 }
 export default Register
